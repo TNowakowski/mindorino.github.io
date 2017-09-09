9cd6729f1c63ebbf0c7bfb33ab143cc9ac7541af
Tomasz Nowakowski <serafin94@tlen.pl>
2017-09-09 09:59:57 +0200

    funkcje

---
+++ javascript/js/zmienne.js
@@ -0,0 +1,13 @@
+'use strict';
+var imie;
+var imieDamskie = "Kaja";
+var wiek =37;
+var kolor = "czerwony";
+
+imie = "sergio";
+
+imie = imieDamskie;
+
+console.log(imie);
+console.log(kolor);
+console.log(wiek);
