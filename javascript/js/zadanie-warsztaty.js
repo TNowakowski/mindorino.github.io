136b57a18b39c8fcfa9fc3fba22420d7ca5bac6f
Tomasz Nowakowski <serafin94@tlen.pl>
2017-09-10 09:10:21 +0200

    zadanie

---
+++ javascript/js/zadanie-warsztaty.js
@@ -0,0 +1,13 @@
+
+    'use strict';
+
+var jeden = 10;
+var dwa = 4;
+var trzy = 6;
+
+function warsztaty(par1,par2,par3) {
+    var wynik;
+    wynik = par1*par2*par3;
+    return wynik;
+}
+var wyswietl = console.log(warsztaty(jeden,dwa,trzy));
